a2f807f08c6ca022fa9a54646037276a6ac12bac
James Orson <jamesaorson@gmail.com>
2020-07-24 19:40:17 -0700

    EMU-2: Add addressing modes

---
+++ Processors/AddressingMode.cs
@@ -0,0 +1,22 @@
+namespace Sharp6502.Processors
+{
+    public enum AddressingMode
+    {
+        Absolute,                 // ABS
+        AbsoluteIndexedX,         // ABS, X
+        AbsoluteIndexedY,         // ABS, Y
+        AbsoluteIndirect,        // (ABS)
+        Accumulator,             // Accum
+        Immediate,               // IMM
+        Implied,                 // Implied
+        IndexedAbsoluteIndirect, // (ABS, X)
+        IndexedIndirect,         // (IND, X)
+        Indirect,                // (IND)
+        IndirectIndexed,         // (IND), Y
+        Relative,                // Relative
+        ZeroPage,                // ZP
+        ZeroPageIndexedX,        // ZP, X
+        ZeroPageIndexedY,        // ZP, Y
+        ZeroPageRelative,        // ZP REL
+    }
+}
